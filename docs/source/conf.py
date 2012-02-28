d55f012e2a9fae92a2d3948f7d5e0909e3f57f90
Simon Weber <sweb090@gmail.com>
2012-02-28 00:47:52 -0500

    mock out 3rd party modules for documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,28 @@
 
 import sys, os
 
+#Mock out 3rd party modules, so documentation can be built.
+#From http://readthedocs.org/docs/read-the-docs/en/latest/faq.html
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(self, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['mutagen', 'mechanize', 'decorator', ]
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
