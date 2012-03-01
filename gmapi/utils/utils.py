e68bc8cfc6f6f2d93cbb21b86e36e35ad98e1cbc
Ryan Hsu <ryanlives@gmail.com>
2012-02-29 16:34:22 -0800

    slim down to_camel_case function

--- gmapi/utils/utils.py
+++ gmapi/utils/utils.py
@@ -42,16 +42,7 @@ except ImportError:
 def to_camel_case(s):
     """Given a sring in underscore form, returns a copy of it in camel case.
     eg, camel_case('test_string') => 'TestString'. """
-
-    ret = string.upper(s[0]) + s[1:]
-
-    ul = re.findall("(_.)", s[1:]) #underscore, then letter
-
-    for to_rep in ul:
-        #replace _j with J
-        ret = ret.replace(to_rep, string.upper(to_rep[1]))
-
-    return ret
+    return ''.join(map(lambda x: x.title(), a.split('_')))
 
 def accept_singleton(expected_type, position=1):
     """Allows a function expecting a list to accept a single item as well.
