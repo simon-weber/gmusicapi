0dff3aad38d84c45547dcf5c24f68b597eccbd3c
Simon Weber <sweb090@gmail.com>
2012-03-27 00:55:02 -0400

    add AUTHORS and packaging info

--- setup.py
+++ setup.py
@@ -2,19 +2,22 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='UnofficialGoogleMusicAPI',
-    version='0.1.0',
+    name='Unofficial-Google-Music-API',
+    version='2012.03.27',
     author='Simon Weber',
-    author_email='sweb090@gmail.com',
+    author_email='simon@simonmweber.com',
+    url='https://github.com/simon-weber/Unofficial-Google-Music-API',
     packages=find_packages(),
-    url='http://pypi.python.org/pypi/TowelStuff/',
+    scripts=['example.py'],
     license='COPYING',
     description='An unofficial api for Google Play Music.',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         "validictory >= 0.8.1",
         "decorator >= 3.3.2",
         "mutagen >= 1.2.0",
         "protobuf >= 2.4.0"
-    ]
+    ],
+    include_package_data=True,
+    zip_safe=False,
 )
