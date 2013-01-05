26f6efb193aec4ff3fdf1f26c705eff3e1b3c303
Simon Weber <sweb090@gmail.com>
2013-01-05 05:08:11 -0500

    release 2013.01.05

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import find_packages
 
 setup(
     name='gmusicapi',
-    version='2012.11.09',
+    version='2013.01.05',
     author='Simon Weber',
     author_email='simon@simonmweber.com',
     url='http://pypi.python.org/pypi/gmusicapi/',
@@ -48,9 +48,9 @@ This api is not supported nor endorsed by Google, and could break at any time.
         'protobuf == 2.4.1',
         'chardet == 2.1.1',
         #for testing album art:
-        'hachoir-core == 1.3.3',
-        'hachoir-parser == 1.3.4',
-        'hachoir-metadata == 1.3.3',
+        #'hachoir-core == 1.3.3',
+        #'hachoir-parser == 1.3.4',
+        #'hachoir-metadata == 1.3.3',
     ],
     classifiers = [
         "Programming Language :: Python",
