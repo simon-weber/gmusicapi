1937424446a08785bede98694f074b25c0f88aa5
Simon Weber <simon@simonmweber.com>
2013-01-26 20:06:34 -0500

    upgrade validictory and requests

--- setup.py
+++ setup.py
@@ -42,12 +42,12 @@ gmusicapi is an unofficial api for Google Play Music. Please see the `project pa
 This api is not supported nor endorsed by Google, and could break at any time.
 """,
     install_requires=[
-        'validictory >= 0.8.3',
+        'validictory == 0.9.0',
         'decorator == 3.3.2',
         'mutagen == 1.20',
         'protobuf == 2.4.1',
         'chardet == 2.1.1',
-        'requests == 1.0.4',
+        'requests == 1.1.0',
         #for testing album art:
         #'hachoir-core == 1.3.3',
         #'hachoir-parser == 1.3.4',
