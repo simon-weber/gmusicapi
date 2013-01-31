5e89baefecce42e677775d3d35dafdba5c9dfd29
Simon Weber <simon@simonmweber.com>
2013-01-31 13:36:49 -0500

    __version__ is now module-level; move to LICENSE

--- gmusicapi/__init__.py
+++ gmusicapi/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""".. moduleauthor:: Simon Weber <sweb090@gmail.com>"""
-
-#TODO title, version, etc. see requests
+__copyright__ = 'Copyright 2013 Simon Weber'
+__license__ = 'BSD 3-Clause'
+__title__ = 'gmusicapi'
+__version__ = '2013.01.05'
 
 from gmusicapi.api import Api
 from gmusicapi.exceptions import CallFailure
--- setup.py
+++ setup.py
@@ -1,46 +1,21 @@
-# Copyright (c) 2012, Simon Weber
-# All rights reserved.
-
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the copyright holder nor the
-#       names of the contributors may be used to endorse or promote products
-#       derived from this software without specific prior written permission.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#!/usr/bin/env python
 
 from distutils.core import setup
 from setuptools import find_packages
 
+import gmusicapi
+
 setup(
     name='gmusicapi',
-    version='2013.01.05',
+    version=gmusicapi.__version__,
     author='Simon Weber',
     author_email='simon@simonmweber.com',
     url='http://pypi.python.org/pypi/gmusicapi/',
     packages=find_packages(),
     scripts=[],
-    license='COPYING',
+    license=open('LICENSE').read(),
     description='An unofficial api for Google Play Music.',
-    long_description="""\
-gmusicapi is an unofficial api for Google Play Music. Please see the `project page <https://github.com/simon-weber/Unofficial-Google-Music-API>`_ for details.
-
-This api is not supported nor endorsed by Google, and could break at any time.
-""",
+    long_description=open('README.md').read(),
     install_requires=[
         'validictory == 0.9.0',
         'decorator == 3.3.2',
@@ -54,12 +29,13 @@ This api is not supported nor endorsed by Google, and could break at any time.
         #'hachoir-metadata == 1.3.3',
     ],
     classifiers=[
-        "Programming Language :: Python",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Topic :: Multimedia :: Sound/Audio",
-        "Topic :: Software Development :: Libraries :: Python Modules"
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     include_package_data=True,
     zip_safe=False,
