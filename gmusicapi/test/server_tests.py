c8d88b5821405cf03635d92940f8fbcccf9602f7
Simon Weber <simon@simonmweber.com>
2013-03-01 11:09:36 -0500

    retry until creation succeeds, note dumb get_playlist_songs behavior

--- gmusicapi/test/server_tests.py
+++ gmusicapi/test/server_tests.py
@@ -64,7 +64,7 @@ class UpauthTests(object):
 
     @test
     def song_create(self):
-        """Create a song to operate on."""
+        """Create a song."""
         self.song_id = None
 
         filepath = test_utils.mp3_filenames[0]
@@ -76,16 +76,48 @@ class UpauthTests(object):
 
         self.song_id = uploaded[filepath]
 
+        # we test get_all_songs here so that we can assume the existance
+        # of the song for future tests (the servers take time to sync an upload)
+        @retry
+        def assert_song_exists(sid):
+            songs = self.api.get_all_songs()
+
+            found = [s for s in songs if s['id'] == sid] or None
+
+            assert_is_not_none(found)
+            assert_equal(len(found), 1)
+
+        assert_song_exists(self.song_id)
+
     @test(depends_on=[song_create])
     def playlist_create(self):
+        """Create a playlist."""
         self.playlist_id = None
 
         self.playlist_id = self.api.create_playlist(TEST_PLAYLIST_NAME)
+
+        # same as song_create, retry until the song appears
+
+        @retry
+        def assert_playlist_exists(plid):
+            playlists = self.api.get_all_playlist_ids()
+
+            with Check() as check:
+                check.equal(sorted(playlists.keys()), ['auto', 'user'])
+                check.equal(playlists['auto'], {})  # see issue 102
+
+                found = playlists['user'].get(TEST_PLAYLIST_NAME, None)
+
+                assert_is_not_none(found)
+                check.equal(found[-1], self.playlist_id)
+
+        assert_playlist_exists(self.playlist_id)
 
     #TODO consider listing/searching if the id isn't there
     # to ensure cleanup.
     @test(depends_on_groups=['playlist'], always_run=True)
     def playlist_delete(self):
+        """Delete the playlist."""
         if self.playlist_id is None:
             raise SkipTest('did not store self.playlist_id')
 
@@ -95,6 +127,7 @@ class UpauthTests(object):
 
     @test(depends_on=[playlist_delete], depends_on_groups=['song'], always_run=True)
     def song_delete(self):
+        """Delete the song."""
         if self.song_id is None:
             raise SkipTest('did not store self.song_id')
 
@@ -139,26 +172,12 @@ class UpauthTests(object):
     ##
 
     @playlist_test
-    @retry
-    def list_playlists(self):
-        playlists = self.api.get_all_playlist_ids()
-
-        with Check() as check:
-            check.equal(sorted(playlists.keys()), ['auto', 'user'])
-            check.equal(playlists['auto'], {})  # see issue 102
-
-            found = playlists['user'].get('gmusicapi_test_playlist', None)
-
-            assert_is_not_none(found)
-            check.equal(found[-1], self.playlist_id)
-
-    @playlist_test
     def change_name(self):
         new_name = TEST_PLAYLIST_NAME + '_mod'
         self.api.change_playlist_name(self.playlist_id, new_name)
 
         @retry  # change takes time to propogate
-        def assert_name_is(plid, name):
+        def assert_name_equal(plid, name):
             playlists = self.api.get_all_playlist_ids()
 
             found = playlists['user'].get(name, None)
@@ -166,8 +185,28 @@ class UpauthTests(object):
             assert_is_not_none(found)
             assert_equal(found[-1], self.playlist_id)
 
-        assert_name_is(self.playlist_id, new_name)
+        assert_name_equal(self.playlist_id, new_name)
 
         # revert
         self.api.change_playlist_name(self.playlist_id, TEST_PLAYLIST_NAME)
-        assert_name_is(self.playlist_id, TEST_PLAYLIST_NAME)
+        assert_name_equal(self.playlist_id, TEST_PLAYLIST_NAME)
+
+    @playlist_test
+    def add_remove(self):
+        @retry
+        def assert_song_order(plid, order):
+            songs = self.api.get_playlist_songs(plid)
+            server_order = [s['id'] for s in songs]
+
+            assert_equal(server_order, order)
+
+        # initially empty
+        assert_song_order(self.playlist_id, [])
+
+        # add two copies
+        self.api.add_songs_to_playlist(self.playlist_id, [self.song_id] * 2)
+        assert_song_order(self.playlist_id, [self.song_id] * 2)
+
+        # remove all copies
+        self.api.remove_songs_from_playlist(self.playlist_id, self.song_id)
+        assert_song_order(self.playlist_id, [])
