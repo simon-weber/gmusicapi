28cd159bf3b937a51d7e10c34720033377c93f9e
Simon Weber <simon@simonmweber.com>
2013-03-29 19:54:00 -0400

    update example

--- example.py
+++ example.py
@@ -3,7 +3,7 @@
 
 from getpass import getpass
 
-from gmusicapi import Api
+from gmusicapi import Webclient
 
 
 def ask_for_credentials():
@@ -11,7 +11,8 @@ def ask_for_credentials():
     Return the authenticated api.
     """
 
-    api = Api()
+    # We're not going to upload anything, so the webclient is what we want.
+    api = Webclient()
 
     logged_in = False
     attempts = 0
@@ -26,7 +27,7 @@ def ask_for_credentials():
     return api
 
 
-def demonstration():
+def demonstrate():
     """Demonstrate some api features."""
 
     api = ask_for_credentials()
@@ -38,8 +39,8 @@ def demonstration():
     print "Successfully logged in."
     print
 
-    #Get all of the users songs.
-    #library is a big list of dictionaries, each of which contains a single song.
+    # Get all of the users songs.
+    # library is a big list of dictionaries, each of which contains a single song.
     print "Loading library...",
     library = api.get_all_songs()
     print "done."
@@ -47,15 +48,15 @@ def demonstration():
     print len(library), "tracks detected."
     print
 
-    #Show some info about a song. There is no guaranteed order;
+    # Show some info about a song. There is no guaranteed order;
     # this is essentially a random song.
     first_song = library[0]
     print "The first song I see is '{}' by '{}'.".format(
         first_song["name"],
         first_song["artist"])
 
-    #We're going to create a new playlist and add a song to it.
-    #Songs are uniquely identified by 'song ids', so let's get the id:
+    # We're going to create a new playlist and add a song to it.
+    # Songs are uniquely identified by 'song ids', so let's get the id:
     song_id = first_song["id"]
 
     print "I'm going to make a new playlist and add that song to it."
@@ -63,28 +64,28 @@ def demonstration():
     print
     playlist_name = raw_input("Enter a name for the playlist: ")
 
-    #Like songs, playlists have unique ids.
-    #Google Music allows more than one playlist of the same name;
+    # Like songs, playlists have unique ids.
+    # Google Music allows more than one playlist of the same name;
     # these ids are necessary.
     playlist_id = api.create_playlist(playlist_name)
     print "Made the playlist."
     print
 
-    #Now lets add the song to the playlist, using their ids:
+    # Now let's add the song to the playlist, using their ids:
     api.add_songs_to_playlist(playlist_id, song_id)
     print "Added the song to the playlist."
     print
 
-    #We're all done! The user can now go and see that the playlist is there.
-    #The web client syncs our changes in real time.
+    # We're all done! The user can now go and see that the playlist is there.
+    # The web client syncs our changes in real time.
     raw_input("You can now check on Google Music that the playlist exists.\n"
               "When done, press enter to delete the playlist:")
     api.delete_playlist(playlist_id)
     print "Deleted the playlist."
 
-    #It's good practice to logout when finished.
+    # It's good practice to logout when finished.
     api.logout()
     print "All done!"
 
 if __name__ == '__main__':
-    demonstration()
+    demonstrate()
