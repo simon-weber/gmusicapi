693746e42d76c0011e00604021c45ab72a728bac
Simon Weber <simon@simonmweber.com>
2014-01-20 17:34:20 -0500

    requests >= 1.1.0

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'decorator >= 3.3.2',
         'mutagen >= 1.20',
         'protobuf >= 2.4.1',
-        'requests >= 1.0.4',
+        'requests >= 1.1.0',               # session.close
         'python-dateutil >= 1.3, != 2.0',  # 2.0 is python3-only
         'proboscis >= 1.2.5.1',   # runs_after
         'oauth2client >= 1.1',    # TokenRevokeError
