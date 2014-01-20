c7a55399db9be91dbe5326a0b69a2ee248e907d2
Simon Weber <simon@simonmweber.com>
2014-01-20 17:38:59 -0500

    protobuf >= 2.4.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'validictory >= 0.8.3',
         'decorator >= 3.3.2',
         'mutagen >= 1.20',
-        'protobuf >= 2.4.1',
+        'protobuf >= 2.4.1',               # 2.3.0 uses ez_setup?
         'requests >= 1.1.0',               # session.close
         'python-dateutil >= 1.3, != 2.0',  # 2.0 is python3-only
         'proboscis >= 1.2.5.1',   # runs_after
