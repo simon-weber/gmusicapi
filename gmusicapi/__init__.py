1d0da3b3bb29bbdabae7c0720ad1df47e2b29664
Simon Weber <simon@simonmweber.com>
2015-06-20 20:11:31 -0400

    remove old Api deprecation code

--- gmusicapi/__init__.py
+++ gmusicapi/__init__.py
@@ -10,19 +10,3 @@ __title__ = 'gmusicapi'
 
 # appease flake8: the imports are purposeful
 (__version__, Webclient, Musicmanager, Mobileclient, CallFailure)
-
-
-class Api(object):
-    """Mock class used to signal gmusicapi.Api deprecation."""
-    def __init__(self):
-        # not using warnings because this change cannot be ignored
-        raise ImportError('gmusicapi.Api is deprecated; use gmusicapi.Webclient'
-                          ' or gmusicapi.Musicmanager instead.'
-                          '\n'
-                          'For help rewriting your code, see'
-                          ' https://unofficial-google-music-api.readthedocs.org/'
-                          'en/latest/usage.html#quickstart.'
-                          '\n'
-                          'For an explanation of why the change was made, see'
-                          ' https://github.com/simon-weber/'
-                          'Unofficial-Google-Music-API/issues/112.')
