d5f5b9c31e759ebb866dcbca84ebab7329b04206
Simon Weber <simon@simonmweber.com>
2016-01-01 21:55:20 -0500

    artistId not required in search results [closes #391]

--- gmusicapi/protocol/mobileclient.py
+++ gmusicapi/protocol/mobileclient.py
@@ -181,7 +181,7 @@ sj_artist = {
         'name': {'type': 'string'},
         'artistArtRef': {'type': 'string', 'required': False},
         'artistBio': {'type': 'string', 'required': False},
-        'artistId': {'type': 'string', 'blank': True},
+        'artistId': {'type': 'string', 'blank': True, 'required': False},
         'albums': {'type': 'array', 'items': sj_album, 'required': False},
         'topTracks': {'type': 'array', 'items': sj_track, 'required': False},
         'total_albums': {'type': 'integer', 'required': False},
